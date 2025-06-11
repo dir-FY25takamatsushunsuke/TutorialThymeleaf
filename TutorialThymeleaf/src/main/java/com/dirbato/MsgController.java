406042f62f39c29339bb4208867e9d99c585342b
dir-FY25takamatsushunsuke <shunsuke.takamatsu.a196@dirbato.co.jp>
2025-06-11 12:29:50 +0900

    o

---
+++ TutorialThymeleaf/src/main/java/com/dirbato/MsgController.java
@@ -0,0 +1,11 @@
+package com.dirbato;
+ import org.springframework.stereotype.Controller;
+ import org.springframework.web.bind.annotation.GetMapping;
+ @Controller
+ public class MsgController{
+ @GetMapping("/msg")
+ public String getMsg() {
+ // msg.htmlに画面遷移
+return "msg";
+ }
+ }
